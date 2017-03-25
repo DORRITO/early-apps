c1a9f849c26a1bb8081d86937d5dd81369bd20e3
CHIp Wright <frisbeeprod@msn.com>
2017-03-25 16:57:19 -0700

    initial commit

---
+++ 8.weather/app/app.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+//es6 descructuring, indexroute and browserhistory are deprecated
+var {Route, Router, HashRouter, Switch} = require('react-router-dom');
+var Main = require('Main');
+var Weather = require('Weather');
+var About = require('About');
+var Examples = require('Examples');
+
+//load foundation
+require('style!css!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
+ReactDOM.render(
+    <HashRouter>                
+        <Main>          
+            <Route path="/about" component={About} />
+            <Route exact path="/" component={Weather} />
+            <Route path="/examples" component={Examples} />
+        </Main>        
+    </HashRouter>,
+    document.getElementById("app")
+);
