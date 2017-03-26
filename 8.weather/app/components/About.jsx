583576165394470ddc47eb8b65b3643569e50f82
CHIp Wright <frisbeeprod@msn.com>
2017-03-26 12:30:45 -0700

    more

--- 8.weather/app/components/About.jsx
+++ 8.weather/app/components/About.jsx
@@ -4,7 +4,7 @@ var About = (props) => {
     return (
         <div>
             <h3>About</h3>
-            <p>welcome to the about page, again, again.</p>
+            <p>welcome to the about page, again, again, again!.</p>
         </div>
     )
 };
