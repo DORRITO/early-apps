e7a8aec569d223f5f7bc1507302b7bbd1505301c
CHIp Wright <frisbeeprod@msn.com>
2017-03-27 03:31:47 -0700

    file swap

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+var {NavLink} = require('react-router-dom');
 
 var Examples = () => {
     return (
-            <h3>Examples</h3>
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <NavLink to='/?location=Yuma'>Yuma, AZ</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/?location=Phoenix'>Phoenix, AZ</NavLink>
+                </li>
+            </ol>
+        </div>
     )
 };
 
