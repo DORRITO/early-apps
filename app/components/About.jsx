53bac810a285c4858a3999743d2ea738331b230c
CHIp Wright <frisbeeprod@msn.com>
2017-03-28 20:13:20 -0700

    removed a p tag

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,6 @@ var About = (props) => {
         <div>
             <h3>About Component</h3>
             <p>welcome to the about page, finally</p>
-            <p>please work</p>
         </div>
     )
 };
